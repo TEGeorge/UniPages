e2b333efa97350c1c71e7497e6a1db48d22b9f57
up719079 <up719079@myport.ac.uk>
2016-01-07 13:08:19 +0000

    -Template For Comment + Input box completed NOW BEING UPDATED USING DOM

--- template/comment.php
+++ template/comment.php
@@ -1,14 +1,12 @@
-<html>
-  <body id="comment">
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h5><a id="comment-author" href="#">Author</a> - <a id="comment-time">HH:MM</a> - <a id="comment-date">DD/MM/YY</a></h4>
-      </div>
-      <div class="panel-body">
-        <p id="comment-content">
-          Hello World
-        </p>
-      </div>
+<body id="comment">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h5><a id="comment-author" href="#">Author</a> - <a id="comment-time">HH:MM</a> - <a id="comment-date">DD/MM/YY</a></h4>
     </div>
-  </body>
-</html>
+    <div class="panel-body">
+      <p id="comment-content">
+        Hello World
+      </p>
+    </div>
+  </div>
+</body>
